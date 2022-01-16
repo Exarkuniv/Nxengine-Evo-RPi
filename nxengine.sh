f99c5904e1b07af664d8a305cec0d60974fd71ef
s1eve-mcdichae1 <92833764+s1eve-mcdichae1@users.noreply.github.com>
2022-01-16 15:28:29 -0800

    These slashes aren't needed.

--- nxengine.sh
+++ nxengine.sh
@@ -33,11 +33,11 @@ function build_nxengine-evo() {
 
     cd ..
     downloadAndExtract "https://www.cavestory.org/downloads/cavestoryen.zip"
-    cp -r cavestoryen/CaveStory/data/ ./
-    cp -r cavestoryen/CaveStory/Doukutsu.exe ./
+    cp -r cavestoryen/CaveStory/data/ .
+    cp -r cavestoryen/CaveStory/Doukutsu.exe .
 
     downloadAndExtract "https://github.com/nxengine/translations/releases/download/v1.14/all.zip" "translations"
-    cp -r translations/data ./
+    cp -r translations/data .
 
     build/nxextract
 }
